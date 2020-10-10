1e35d5be20883352be8416a3674118dc30e14d40
gx1234 <296312446@qq.com>
2020-10-10 11:24:44 +0800

    axios封装

---
+++ src/api/request.js
@@ -0,0 +1,64 @@
+//对axios的二次封装
+//第一步 导入
+import axios from "axios";
+//基础配置
+// axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.timeout = 100000;
+
+// function abc() {
+//   return axios.get("/api/homecasual");
+// }
+// console.log(abc);
+// //网络请求的拦截器
+// //请求拦截器
+// axios.interceptors.request.use(
+//   function(config) {
+//     return config;
+//   },
+//   function(error) {
+//     return Promise.reject(error);
+//   }
+// );
+//
+// // 响应拦截器
+// axios.interceptors.response.use(
+//   function(response) {
+//     return response.data;
+//   },
+//   function(error) {
+//     return Promise.reject(error);
+//   }
+// );
+
+//请求方式进行一次封装
+function $http(url, data = {}, method = "GET") {
+  // let promise = null;
+  // if (method === "GET") {
+  //   promise = axios.get(url, {
+  //     params: data
+  //   });
+  // } else if (method === "POST") {
+  //   axios.post(url, data);
+  // }
+  // return promise;
+  let promise = null;
+  return new Promise(function(resolve, reject) {
+    if (method === "GET") {
+      promise = axios.get(url, {
+        params: data
+      });
+    } else if (method === "POST") {
+      promise = axios.post(url, data);
+    }
+    promise
+      .then(res => {
+        resolve(res);
+      })
+      .catch(error => {
+        reject(error);
+      });
+    return promise;
+  });
+}
+
+export default $http;
