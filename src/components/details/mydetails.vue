00bad771de5e72b357aee47c646961806f01b809
py <18234866370@163.com>
2020-10-14 22:19:24 +0800

    首页详情购物车完工

---
+++ src/components/details/mydetails.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="details">
+  
+  </div>
+</template>
+
+<script>
+  import {homedata} from "../../api";
+
+  export default {
+    name: "details",
+    data(){
+      return{
+        msg:[],
+        id:''
+      }
+    },
+    mounted() {
+      this.id=this.$router.query.id
+      this.getdata()
+    },
+    methods:{
+      getdata(){
+        homedata().then(res=>{
+          console.log(res)
+          this.msg=res
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
